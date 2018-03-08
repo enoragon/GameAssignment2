8c2d79d6d6c5d8eab82d12e65b01100309135b01
Unknown <enoragon@gmail.com>
2018-03-07 21:50:15 -0600

    Settup Scene

---
+++ Pee Simulator/Assets/body.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class body : MonoBehaviour {
+    public float speed = 0.1f;
+    public float edgeBounds = 10f;
+    public float chanceToChangeDirection = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+
+    }
+}
